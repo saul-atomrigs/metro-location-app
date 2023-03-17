8b888339d07fb5b3db0167f435f192007193726a
Saul Lee <solleedata@gmail.com>
2023-03-17 14:02:10 +0900

    feat: Implement locale feature for multi-language

--- app/App.tsx
+++ app/App.tsx
@@ -1,10 +1,46 @@
 import React from 'react';
-import {SafeAreaView, useColorScheme} from 'react-native';
+import {SafeAreaView, useColorScheme, I18nManager, Text} from 'react-native';
 import {colors} from 'styles/common';
 import Home from 'components/view/Home';
+import * as RNLocalize from 'react-native-localize';
+import {createIntl, createIntlCache} from '@formatjs/intl';
+
+const translations = {
+  kr: require('../locale/kr.json'),
+  en: require('../locale/en.json'),
+} as const;
+
+export type Translation = keyof typeof translations;
+
+// fallback if no available language fits
+const fallback = {languageTag: 'en', isRTL: false};
+
+const {languageTag, isRTL} =
+  RNLocalize.findBestAvailableLanguage(Object.keys(translations)) ?? fallback;
+
+// update layout direction
+I18nManager.forceRTL(isRTL);
+
+const intl = createIntl(
+  {
+    defaultLocale: 'en',
+    locale: languageTag,
+    messages: translations[languageTag as Translation],
+  },
+  createIntlCache(),
+);
+
+type TranslationParams = Parameters<(typeof intl)['formatMessage']>[1];
+
+const translate = (key: string, params?: TranslationParams) =>
+  intl
+    .formatMessage({id: key, defaultMessage: translations['en'][key]}, params)
+    .toString();
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  console.log(RNLocalize.getLocales());
+  console.log(translate('hello'));
 
   const backgroundStyle = {
     backgroundColor: isDarkMode
@@ -15,6 +51,7 @@ function App(): JSX.Element {
   return (
     <SafeAreaView style={backgroundStyle}>
       <Home />
+      <Text>{translate('hello')}</Text>
     </SafeAreaView>
   );
 }
