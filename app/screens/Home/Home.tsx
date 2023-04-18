890dfa54915ad2c3fb928b07f8d420085c974c47
Saul Lee <solleedata@gmail.com>
2023-04-18 11:57:24 +0900

    feat: react-native-dotenv 적용

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -3,7 +3,7 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import {KeyboardAvoidingView, TextInput, Keyboard} from 'react-native';
-import Config from 'react-native-config';
+import {SEOUL_METRO_API_KEY} from '@env';
 import NaverMapView, {
   Circle,
   Marker,
@@ -16,11 +16,10 @@ import styled from 'styled-components/native';
 import isIos from 'util/device';
 
 export default function Home() {
-  const URL = `http://openapi.seoul.go.kr:8088/${Config.SEOUL_METRO_API_KEY}/json/subwayStationMaster/1/5/`;
+  const URL = `http://openapi.seoul.go.kr:8088/${SEOUL_METRO_API_KEY}/json/subwayStationMaster/1/5/`;
   const INITIAL_POSITION = {latitude: 37.570161, longitude: 126.982923};
-  console.log('URL', URL);
 
-  const [metroData, setMetroData] = useState();
+  const [metroData, setMetroData] = useState([]);
   const [P0, setP0] = useState(INITIAL_POSITION);
 
   useEffect(() => {
@@ -30,13 +29,12 @@ export default function Home() {
   const getMetroData = async () => {
     try {
       const response = await axios.get(URL);
-      const rawData = response.data.subwayStationMaster.row;
-      setMetroData(rawData);
+      const metroRowData = response.data.subwayStationMaster.row;
+      setMetroData(metroRowData);
       setP0({
-        latitude: Number(metroData[1].CRDNT_Y),
-        longitude: Number(metroData[1].CRDNT_X),
+        latitude: Number(metroData[3].CRDNT_Y),
+        longitude: Number(metroData[3].CRDNT_X),
       });
-      console.log(P0);
     } catch (error) {
       console.log('error', error);
     }
