3b5828034627a170c7448897f866570b90e95400
Saul Lee <solleedata@gmail.com>
2023-04-20 17:19:39 +0900

    feat: react-hook-form 을 네이버 지도와 연결 (검색 기능)

--- app/screens/Home/Home.types.ts
+++ app/screens/Home/Home.types.ts
@@ -2,3 +2,15 @@ export type MetroDataProps = {
   CRDNT_Y: string;
   CRDNT_X: string;
 };
+
+export type MetroRowData = {
+  CRDNT_X: string;
+  CRDNT_Y: string;
+  STATN_ID: string;
+  ROUTE: string;
+  STATN_NM: string;
+};
+
+export type SearchResult = {
+  searchResult: string;
+};
