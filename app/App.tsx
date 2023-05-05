8c118cdd6dd9a30482f0a56397983cdba83bdc72
Saul Lee <solleedata@gmail.com>
2023-05-05 10:53:27 +0900

    refactor: App.tsx to Home.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {SafeAreaView, Text, useColorScheme} from 'react-native';
+import {SafeAreaView, useColorScheme} from 'react-native';
 import {RecoilRoot} from 'recoil';
-import Geolocation from 'react-native-geolocation-service';
 
 import Home from 'screens/Home';
 import {colors} from 'styles/common';
-import {translate as t} from 'locale/';
-import {requestPermissions} from 'util/geolocation';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -14,20 +11,6 @@ export default function App() {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? colors.black : colors.white,
   };
-
-  React.useEffect(() => {
-    requestPermissions();
-    Geolocation.getCurrentPosition(
-      position => {
-        console.log(position);
-      },
-      error => {
-        // See error code charts below.
-        console.log(error.code, error.message);
-      },
-      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-    );
-  }, []);
 
   return (
     <RecoilRoot>
