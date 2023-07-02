bca3937a6caf40543fb917eb85043287dbd1abc8
Saul Lee <saul@atomrigs.io>
2023-07-03 00:07:34 +0900

    refactor: 임포트 정리, 미사용 코드 제거

--- app/screens/Home/Home.tsx
+++ app/screens/Home/Home.tsx
@@ -19,22 +19,15 @@ import NaverMapView, {
 } from 'react-native-nmap';
 import styled from 'styled-components/native';
 import {useForm, Controller} from 'react-hook-form';
-import {RecoilState, atom, useRecoilState} from 'recoil';
 import Geolocation from 'react-native-geolocation-service';
 import notifee, {AndroidImportance} from '@notifee/react-native';
 
 import {MetroDataProps, MetroRowData, SearchResult} from './Home.types';
 import {URL, INITIAL_POSITION} from './Home.constants';
-// import Text from 'component-library/Text';
-import SearchResultsList from 'components/SearchResultsList';
 import {requestPermissions} from 'util/geolocation';
 import isIos from 'util/device';
 
 export default function Home() {
-  /** 지하철역 좌표 */
-  const [metroData, setMetroData] = useState<MetroDataProps[]>([]);
-  const [P0, setP0] = useState(INITIAL_POSITION);
-
   /** 유저의 현재 Geolocation 좌표 */
   const [currentPosition, setCurrentPosition] = useState<any>({
     coords: {
@@ -43,6 +36,14 @@ export default function Home() {
     },
   });
   const {latitude, longitude} = currentPosition?.coords || {};
+
+  /** 지하철역 좌표 */
+  const [metroData, setMetroData] = useState<MetroDataProps[]>([]);
+  const [P0, setP0] = useState(INITIAL_POSITION);
+  // const [P0, setP0] = useState({
+  //   latitude: latitude,
+  //   longitude: longitude,
+  // });
 
   useEffect(() => {
     requestPermissions();
@@ -132,37 +133,6 @@ export default function Home() {
     }
   };
 
-  // const onUpdateNotification = async () => {
-  //   try {
-  //     // (required for Android)
-  //     const channelId = await notifee.createChannel({
-  //       id: 'default',
-  //       name: 'Default Channel',
-  //       importance: AndroidImportance.HIGH,
-  //     });
-
-  //     await notifee.displayNotification({
-  //       id: '123',
-  //       title: '지하철 앱',
-  //       body: '이번 역에서 내리세요',
-  //       android: {
-  //         channelId,
-  //         asForegroundService: true, // registerForegroundService에서 등록한 runner에 bound됨
-  //         // open the app when pressed
-  //         pressAction: {
-  //           id: 'default',
-  //         },
-  //       },
-  //     });
-  //   } catch (error: any) {
-  //     Alert.alert('error', error);
-  //   }
-  // };
-
-  // if (isTargetedStation) {
-  //   onUpdateNotification();
-  // }
-
   /** FOREGROUND SERVICE 멈춤: */
   const stopForegroundService = async () => {
     try {
@@ -180,35 +150,30 @@ export default function Home() {
   };
 
   notifee.registerForegroundService(() => {
-    return new Promise(async () => {
+    return new Promise(() => {
       Geolocation.watchPosition(async position => {
         let currentLatitude = position.coords.latitude;
         let currentLongitude = position.coords.longitude;
         if (
-          // Math.abs(currentLatitude - 37.571449) < 0.005 &&
-          // Math.abs(currentLongitude - 126.73578) < 0.005
-          isTargetedStation
+          Math.abs(currentLatitude - P0.latitude) < 0.005 &&
+          Math.abs(currentLongitude - P0.longitude) < 0.005
         ) {
           const channelId = await notifee.createChannel({
             id: 'default',
             name: 'Default Channel',
             importance: AndroidImportance.HIGH,
           });
-
           notifee.displayNotification({
             id: '123',
             title: '지하철 앱',
             body: '이번 역에서 내리세요',
             android: {
               channelId,
-              asForegroundService: true, // registerForegroundService에서 등록한 runner에 bound됨
               pressAction: {
                 id: 'default',
               },
             },
           });
-
-          await notifee.stopForegroundService();
         }
       });
     });
@@ -218,47 +183,6 @@ export default function Home() {
 
   return (
     <KeyboardAvoidingView behavior={isIos ? 'padding' : 'height'}>
-      <Button
-        title="Display Notification"
-        onPress={() => onDisplayNotification()}
-      />
-      <Button
-        title="getCurrentPosition"
-        onPress={() =>
-          Geolocation.getCurrentPosition(position => {
-            setCurrentPosition(position);
-          })
-        }
-      />
-      <Button title="stopForegroundService" onPress={stopForegroundService} />
-      <Button title="onBackgroundEvent" onPress={onBackgroundEvent} />
-      <NaverMapView
-        style={{width: '100%', height: '60%'}}
-        showsMyLocationButton={false}
-        center={{
-          // latitude,
-          // longitude,
-          ...P0,
-          zoom: 16,
-        }}
-        // 해당하는 좌표로 화면을 이동:
-        // animateToCoordinate={{
-        //   latitude,
-        //   longitude,
-        // }}
-        onTouch={() => {}}
-        onMapClick={() => Keyboard.dismiss()}>
-        <Marker
-          coordinate={
-            // latitude,
-            // longitude,
-            P0
-          }
-          onClick={() => console.warn('onClick! p0')}
-        />
-      </NaverMapView>
-      <Text>{latitude}</Text>
-      <Text>{longitude}</Text>
       <Controller
         control={control}
         rules={{
@@ -275,9 +199,46 @@ export default function Home() {
         name="searchResult"
       />
       {errors.searchResult && <Text>This is required.</Text>}
-      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-      <Text>{P0.latitude}</Text>
-      <Text>{P0.longitude}</Text>
+      <Button title="도착역 찾기" onPress={handleSubmit(onSubmit)} />
+
+      <Button title="알림 설정" onPress={() => onDisplayNotification()} />
+      <Button title="알림 해제" onPress={stopForegroundService} />
+      <NaverMapView
+        style={{width: '100%', height: '60%'}}
+        showsMyLocationButton={false}
+        center={{
+          latitude: latitude,
+          longitude: longitude,
+          // ...P0,
+          zoom: 16,
+        }}
+        // 해당하는 좌표로 화면을 이동:
+        // animateToCoordinate={{
+        // latitude,
+        // longitude,
+        // P0,
+        // }}
+        onTouch={() => {}}
+        onMapClick={() => Keyboard.dismiss()}>
+        <Marker
+          coordinate={
+            // 지하철역에 마커를 찍는다:
+            // {latitude, longitude}
+            P0
+          }
+          onClick={() => console.warn('onClick! p0')}
+        />
+        <Circle coordinate={P0} radius={300} color={'rgba(255,0,0,0.3)'} />
+      </NaverMapView>
+      <Text>현재위치: {latitude}</Text>
+      <Text>현재위치: {longitude}</Text>
+      <Text>{String(isTargetedStation)}</Text>
+      <Text>
+        타깃 역:{P0.latitude} ({P0.latitude - latitude})
+      </Text>
+      <Text>
+        타깃 역:{P0.longitude} ({P0.longitude - longitude})
+      </Text>
     </KeyboardAvoidingView>
   );
 }
