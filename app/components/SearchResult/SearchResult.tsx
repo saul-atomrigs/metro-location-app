ae921a1812b18d49c83b35a0c0af0b4f397ff92e
Saul Lee <saul@atomrigs.io>
2023-07-29 11:43:04 +0900

    feat: 스타일링 적용 (styled components)

--- app/components/SearchResult/SearchResult.tsx
+++ app/components/SearchResult/SearchResult.tsx
@@ -1,15 +1,34 @@
-import {Text, View} from 'react-native';
 import React from 'react';
-// import Text from 'component-library/Text';
+import {Text} from 'react-native';
+import styled from 'styled-components/native';
 
 type SearchResultProps = {
   result: string;
+  searchText: string;
+  setSearchText: (text: string) => void;
+  getMetroData: () => void;
 };
 
-export default function SearchResult({result}: SearchResultProps) {
+export default function SearchResult({
+  result,
+  setSearchText,
+  getMetroData,
+}: SearchResultProps) {
+  const onPressHandler = () => {
+    setSearchText(result);
+    getMetroData(result);
+  };
+
   return (
-    <View>
+    <SearchResultBox
+      // onPress={onPress}
+      onPress={onPressHandler}>
       <Text>{result}</Text>
-    </View>
+    </SearchResultBox>
   );
 }
+
+const SearchResultBox = styled.TouchableOpacity`
+  margin-bottom: 20px;
+  margin-horizontal: 5px;
+`;
