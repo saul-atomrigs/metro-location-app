6e3142afee5f717c89eb4de2198bce8227ad5e9b
Saul Lee <saul@atomrigs.io>
2023-08-20 22:17:59 +0900

    feat: 튜토리얼 페이지 뼈대 작성

--- app/App.tsx
+++ app/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {SafeAreaView, useColorScheme} from 'react-native';
 import {RecoilRoot} from 'recoil';
 
-import Home from 'screens/Home';
-import {colors} from 'styles/common';
+import Home from './screens/Home';
+import Tutorial from './screens/tutorial/Tutorial';
+import {colors} from './styles/common';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -16,7 +17,7 @@ export default function App() {
   return (
     <RecoilRoot>
       <SafeAreaView style={backgroundStyle}>
-        <Home />
+        <Tutorial />
       </SafeAreaView>
     </RecoilRoot>
   );
---
+++ app/screens/tutorial/Tutorial.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function Tutorial() {
+  return (
+    <>
+      {/* # 제목: 위치 정보 수집을 위한 권한 요청 */}
+      <Text>위치 정보 수집을 위한 권한 요청</Text>
+
+      {/* 내리세요 앱의 필수 기능을 위해 귀하의 위치 정보를 수집합니다... */}
+      <Text>
+        내리세요 앱의 필수 기능을 위해 귀하의 위치 정보를 수집합니다...
+      </Text>
+
+      {/* lottie 지도 애니메이션 */}
+
+      {/* 동의하고 시작하기 버튼 */}
+    </>
+  );
+}
