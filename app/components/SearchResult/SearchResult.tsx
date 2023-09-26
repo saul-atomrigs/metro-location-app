6dbc6dafe2d95f513c9415cd8b481bc7db26678a
Saul Lee <saul@atomrigs.io>
2023-09-26 15:35:22 +0900

    fix: duplicated search results

---
+++ app/components/SearchResult/SearchResult.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Text} from 'react-native';
+import styled from 'styled-components/native';
+
+type SearchResultProps = {
+  result: string;
+  metroLine: string;
+  searchText: string;
+  setSearchText: (text: string) => void;
+  getMetroData: (text: string) => void;
+};
+
+export default function SearchResult({
+  result,
+  metroLine,
+  setSearchText,
+  getMetroData,
+}: SearchResultProps) {
+  const onPressHandler = () => {
+    setSearchText(result);
+    getMetroData(result);
+  };
+
+  return (
+    <SearchResultBox
+      // onPress={onPress}
+      onPress={onPressHandler}>
+      <Text>
+        {result} ({metroLine})
+      </Text>
+    </SearchResultBox>
+  );
+}
+
+const SearchResultBox = styled.TouchableOpacity`
+  margin: 10px;
+`;
---
+++ app/components/SearchResultsList/SearchResultsList.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {View} from 'react-native';
+import styled from 'styled-components/native';
+
+import SearchResult from 'components/SearchResult/SearchResult';
+import {MetroRowData} from 'screens/Home/Home.types';
+
+type SearchResultsListProps = {
+  metroData: MetroRowData[];
+  searchText: string;
+  setSearchText: (text: string) => void;
+  getMetroData: () => void;
+};
+
+export default function SearchResultsList({
+  metroData,
+  searchText,
+  setSearchText,
+  getMetroData,
+  P0,
+  setP0,
+}: SearchResultsListProps) {
+  console.log('P0 --', P0);
+  return (
+    metroData && (
+      <SearchResults>
+        {metroData
+          .filter(station => {
+            if (searchText === '') {
+              return null;
+            } else if (
+              station.STATN_NM.toLowerCase().includes(searchText.toLowerCase())
+            ) {
+              return station;
+            }
+          })
+          .map(station => {
+            return (
+              <View>
+                <SearchResult
+                  setSearchText={setSearchText}
+                  searchText={searchText}
+                  result={station.STATN_NM}
+                  metroLine={station.ROUTE}
+                  getMetroData={getMetroData}
+                  P0={P0}
+                  setP0={setP0}
+                />
+              </View>
+            );
+          })}
+      </SearchResults>
+    )
+  );
+}
+
+const SearchResults = styled.View`
+  position: absolute;
+  top: 40px;
+  z-index: 1;
+  background-color: #fff;
+  width: 100%;
+`;
