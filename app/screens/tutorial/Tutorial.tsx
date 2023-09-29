ac73d247858a8b12e98b08102ae6b44b5250472c
Saul Lee <saul@atomrigs.io>
2023-09-29 17:04:56 +0900

    feat: lottie animation in tutorial screen

--- app/screens/tutorial/Tutorial.tsx
+++ app/screens/tutorial/Tutorial.tsx
@@ -1,20 +1,77 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
+import styled from 'styled-components/native';
+import {useNavigation} from '@react-navigation/native';
+
+import MapAnimation from 'components/map-animation/MapAnimation';
+import {requestGeolocationPermissions} from 'util/geolocation';
 
 export default function Tutorial() {
+  const [isGeolocationPermissionGranted, setIsGeolocationPermissionGranted] =
+    useState(false);
+
+  const navigation = useNavigation();
+
+  const handleAgreePress = async () => {
+    const result = await requestGeolocationPermissions();
+    setIsGeolocationPermissionGranted(result);
+  };
+
+  useEffect(() => {
+    if (isGeolocationPermissionGranted) {
+      navigation.navigate('Home');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isGeolocationPermissionGranted]);
+
   return (
-    <>
-      {/* # 제목: 위치 정보 수집을 위한 권한 요청 */}
-      <Text>위치 정보 수집을 위한 권한 요청</Text>
+    <TutorialContainer>
+      <View>
+        <TutorialTitle>위치 정보 수집을 위한 권한 요청</TutorialTitle>
 
-      {/* 내리세요 앱의 필수 기능을 위해 귀하의 위치 정보를 수집합니다... */}
-      <Text>
-        내리세요 앱의 필수 기능을 위해 귀하의 위치 정보를 수집합니다...
-      </Text>
+        <TutorialDescription>
+          내리세요 앱의 필수 기능을 위해 귀하의 위치 정보를 수집합니다.
+        </TutorialDescription>
+      </View>
 
-      {/* lottie 지도 애니메이션 */}
+      <MapAnimation />
 
-      {/* 동의하고 시작하기 버튼 */}
-    </>
+      <Button onPress={handleAgreePress}>
+        <ButtonText>위치 정보 수집에 동의합니다.</ButtonText>
+      </Button>
+    </TutorialContainer>
   );
 }
+
+const TutorialContainer = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  background-color: #fff;
+  padding: 20px;
+`;
+
+const TutorialTitle = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #000;
+  margin-bottom: 10px;
+`;
+
+const TutorialDescription = styled.Text`
+  font-size: 16px;
+  line-height: 22px;
+  color: #2d2c2c;
+  margin-bottom: 20px;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: #190c8d;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+`;
