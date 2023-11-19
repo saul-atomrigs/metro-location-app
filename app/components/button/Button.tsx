53b5ed3bdd61742dedf2e435859cda2871c56c39
Saul Lee <solleedata@gmail.com>
2023-11-20 00:46:58 +0900

    refactor: Button styled component

--- app/components/button/Button.tsx
+++ app/components/button/Button.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
 import styled from 'styled-components/native';
+
 import type {ButtonProps} from './Button.types';
 
-export default function Button({onPress, title, buttonStyle}: ButtonProps) {
+export default function ButtonComponent({
+  onPress,
+  title,
+  buttonStyle,
+}: ButtonProps) {
   return (
-    <PrimaryButton onPress={onPress} buttonStyle={buttonStyle}>
-      <PrimaryButtonText>{title}</PrimaryButtonText>
-    </PrimaryButton>
+    <Button.Body onPress={onPress} buttonStyle={buttonStyle}>
+      <Button.Text>{title}</Button.Text>
+    </Button.Body>
   );
 }
 
-const PrimaryButton = styled.TouchableOpacity<{
-  buttonStyle: 'primary' | 'danger';
-}>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-self: center;
-  position: relative;
-  bottom: 25%;
-  height: 50px;
-  width: 90%;
-  margin-bottom: 10px;
-  background-color: ${({buttonStyle}) =>
-    buttonStyle === 'primary' ? '#190c8d' : '#b50404'};
-  border-radius: 13px;
-`;
+const Button = {
+  Body: styled.TouchableOpacity<{
+    buttonStyle: 'primary' | 'danger';
+  }>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    position: relative;
+    bottom: 25%;
+    height: 50px;
+    width: 90%;
+    margin-bottom: 10px;
+    background-color: ${({buttonStyle}) =>
+      buttonStyle === 'primary' ? '#190c8d' : '#b50404'};
+    border-radius: 13px;
+  `,
 
-const PrimaryButtonText = styled.Text`
-  color: #ffffff;
-  font-size: 16px;
-  font-weight: 600;
-`;
+  Text: styled.Text`
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: 600;
+  `,
+};
